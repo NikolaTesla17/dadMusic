4c1de8a695a18fa34f3e7138f8c65efa70fd44fe
NikolaTesla17 <44534701+NikolaTesla17@users.noreply.github.com>
2020-06-20 12:54:35 -0700

    Update script.js

--- script.js
+++ script.js
@@ -521,7 +521,7 @@ var Framer = {
             {
                 artist: "Me",
                 song: "lookee here",
-                url: "http://www.hochmuth.com/mp3/Haydn_Cello_Concerto_D-1.mp3"
+                url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"
             }
         ],
 
